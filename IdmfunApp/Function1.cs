0fa30b898d30721cac2f26b14c8b254b27a4b5f3
Siva <pokuri86@gmail.com>
2019-04-17 12:09:34 -0400

    Text changes

--- IdmfunApp/Function1.cs
+++ IdmfunApp/Function1.cs
@@ -27,7 +27,7 @@ namespace IdmfunApp
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"Hello there, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
